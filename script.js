d2cf80e2d25079af218907bc92421da731e8eaa5
Henry Kam <h3nrykam@gmail.com>
2021-09-22 20:23:20 -0700

    create input logic

--- script.js
+++ script.js
@@ -1,6 +1,91 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//make strings of lowercase, uppercase, numeric, special chars
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890";
+var specialChars = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~\\";
+
+function generatePassword() // added function
+{
+  var length = -1;
+  //prompt user for length of password (must be at least 8 characters and less than 128 characters)
+  while(true)
+  {
+    length = parseInt(window.prompt("Enter length of password (min: 8 characters max: 128):","8"));
+    if(!isNaN(length))
+      if(length>=8 && length<=128)
+        break;
+      else
+        window.alert("must fit within 8 to 128 characters!");
+    else
+      window.alert("Invalid input! Try again.");
+  }
+  //prompt user for lowercase characters
+  var low = false;
+  while(true)
+  {
+    var ans = window.prompt("Do you want lowercase characters? (yes/no)", yes).toLowerCase();
+    if(ans=="yes")
+    {
+      low = true;
+      break;
+    }
+    else if(ans == "no")
+      break;
+    else
+      window.alert("Invalid input! Try again.");
+  }
+  //prompt user for uppercase characters
+  var up = false;
+  while(true)
+  {
+    var ans = window.prompt("Do you want uppercase characters? (yes/no)", yes).toLowerCase();
+    if(ans=="yes")
+    {
+      up = true;
+      break;
+    }
+    else if(ans == "no")
+      break;
+    else
+      window.alert("Invalid input! Try again.");
+  }
+  //prompt user for numeric characters
+  var num = false;
+  while(true)
+  {
+    var ans = window.prompt("Do you want numeric characters? (yes/no)", yes).toLowerCase();
+    if(ans=="yes")
+    {
+      num = true;
+      break;
+    }
+    else if(ans== "no")
+      break;
+    else
+      window.alert("Invalid input! Try again.");
+  }
+  //prompt user for special characters  !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+  var spec = false;
+  while(true)
+  {
+    var ans = window.prompt("Do you want special characters? (yes/no)", yes).toLowerCase();
+    if(ans=="yes")
+    {
+      spec = true;
+      break;
+    }
+    else if(ans== "no")
+      break;
+    else
+      window.alert("Invalid input! Try again.");
+  }
+
+  
+  return length;
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
